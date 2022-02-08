9adfa4eefb83e35cda82b90ef7e5c5392d55e24f
DiegoEDG <1.5.dg.diego@gmail.com>
2022-02-08 12:57:06 -0600

    Json Server Added

--- src/components/NuevoProducto.jsx
+++ src/components/NuevoProducto.jsx
@@ -1,5 +1,28 @@
 import React from 'react';
 
 export const NuevoProducto = () => {
-	return <div>Nuevo Producto</div>;
+	return (
+		<div className="row justify-content-center">
+			<div className="col-md-8">
+				<div className="card">
+					<div className="card-body">
+						<h2 className="text-center mb-3 font-weight-bold">Agregar Nuevo Producto</h2>
+						<form>
+							<div className="form-group">
+								<label htmlFor="name">Nombre Producto</label>
+								<input type="text" name="name" className="form-control" placeholder="Nombre Producto" />
+							</div>
+							<div className="form-group">
+								<label htmlFor="price">Nombre Producto</label>
+								<input type="number" name="price" className="form-control" placeholder="Precio Producto" />
+							</div>
+							<button type="submit" className="btn btn-primary font-weight-bold text-uppercase d-block w-100">
+								Agregar
+							</button>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
--- src/components/Productos.jsx
+++ src/components/Productos.jsx
@@ -1,5 +1,19 @@
 import React from 'react';
 
 export const Productos = () => {
-	return <div>Productos</div>;
+	return (
+		<>
+			<h2 className="text-center my-3">Listado de productos</h2>
+			<table className="table table-striped">
+				<thead className="bg-primary table-dark">
+					<tr>
+						<th scope="col">Nombre</th>
+						<th scope="col">Precio</th>
+						<th scope="col">Acciones</th>
+					</tr>
+				</thead>
+				<tbody></tbody>
+			</table>
+		</>
+	);
 };
