2768bba056ab25013247adbe6020d4f98c9ede5a
DiegoEDG <1.5.dg.diego@gmail.com>
2022-02-08 16:45:32 -0600

    First Actions Added

--- src/actions/productAction.js
+++ src/actions/productAction.js
@@ -1,8 +1,29 @@
 import { ADD_PRODUCT, ADD_PRODUCT_SUCCESS, ADD_PRODUCT_ERROR } from '../types';
 
 //crear productos
-export function newProductAction() {
-	return () => {
-		console.log('Desde newProductAction');
+export function newProductAction(product) {
+	return (dispatch) => {
+		dispatch(addNewProduct());
+
+		try {
+			dispatch(addProductSuccess(product));
+		} catch (error) {
+			dispatch(addProductError(true));
+		}
 	};
 }
+
+const addNewProduct = () => ({
+	type: ADD_PRODUCT,
+	payload: true
+});
+
+const addProductSuccess = (product) => ({
+	type: ADD_PRODUCT_SUCCESS,
+	payload: product
+});
+
+const addProductError = (payload) => ({
+	type: ADD_PRODUCT_ERROR,
+	payload
+});
--- src/components/NuevoProducto.jsx
+++ src/components/NuevoProducto.jsx
@@ -1,22 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 //actions
 import { newProductAction } from '../actions/productAction';
+import { useDispatch, useSelector } from 'react-redux';
 
 export const NuevoProducto = () => {
+	const [product, setProduct] = useState({
+		name: '',
+		price: 0
+	});
+	const dispatch = useDispatch();
+
+	const handleProduct = (e) => {
+		setProduct({ ...product, [e.target.name]: e.target.value });
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+
+		product.price = Number(product.price);
+		dispatch(newProductAction(product));
+	};
+
 	return (
 		<div className="row justify-content-center">
 			<div className="col-md-8">
 				<div className="card">
 					<div className="card-body">
 						<h2 className="text-center mb-3 font-weight-bold">Agregar Nuevo Producto</h2>
-						<form>
+						<form onSubmit={(e) => handleSubmit(e)}>
 							<div className="form-group">
 								<label htmlFor="name">Nombre Producto</label>
-								<input type="text" name="name" className="form-control" placeholder="Nombre Producto" />
+								<input
+									type="text"
+									name="name"
+									className="form-control"
+									placeholder="Nombre Producto"
+									value={product.name}
+									onChange={(e) => handleProduct(e)}
+								/>
 							</div>
 							<div className="form-group">
 								<label htmlFor="price">Nombre Producto</label>
-								<input type="number" name="price" className="form-control" placeholder="Precio Producto" />
+								<input
+									type="number"
+									name="price"
+									className="form-control"
+									placeholder="Precio Producto"
+									value={product.price}
+									onChange={(e) => handleProduct(e)}
+								/>
 							</div>
 							<button type="submit" className="btn btn-primary font-weight-bold text-uppercase d-block w-100">
 								Agregar
